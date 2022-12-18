437ad66e03627e247d46a735dd268af9c2a05648
jungbum <jungbum05114@naver.com>
2022-12-18 22:28:17 +0900

    setting: base

---
+++ src/main/java/com/TIA/TiaApplication.java
@@ -0,0 +1,13 @@
+package com.TIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TIA/TiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
